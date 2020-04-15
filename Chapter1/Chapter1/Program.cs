fc3b4114488b8d9a436760c604f9c86cb52695dc
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2020-04-15 16:37:02 -0500

    Bruh

---
+++ Chapter1/Chapter1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXX                              XXXX");
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+            Console.WriteLine("XXXX           XXXX XXXX          XXXX");
+        }
+    }
+}
